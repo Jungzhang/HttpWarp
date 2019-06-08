ecbe2475721e80d9e22be1de655ece6947bc121b
Jungzhang <zhanggen.jung@gmail.com>
2019-06-08 17:47:10 +0800

    修改package name

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"os"
-	"github.com/Jungzhang/HttpWarp/server"
-	"github.com/Jungzhang/HttpWarp/client"
+	"github.com/Jungzhang/HttpWarp/servers"
+	"github.com/Jungzhang/HttpWarp/clients"
 )
 
 // 如果可执行文件名为server则执行server端进程
 // 否则执行client
 func main() {
 	if os.Args[0] == "server" {
-		server.Start()
+		servers.Start()
 	} else {
-		client.Start()
+		clients.Start()
 	}
 }
