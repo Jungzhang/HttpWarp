183631683ee4bcf50084f5850932e7b306ee4d1a
Jungzhang <zhanggen.jung@gmail.com>
2019-06-09 20:12:35 +0800

    fix bug : server 启动

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"os"
 	"github.com/Jungzhang/HttpWarp/servers"
 	"github.com/Jungzhang/HttpWarp/clients"
+	"runtime"
+	"strings"
 )
 
 // 如果可执行文件名为server则执行server端进程
 // 否则执行client
 func main() {
-	if os.Args[0] == "server" {
+
+	split := "/"
+	if runtime.GOOS == "windows" {
+		split = "\\"
+	}
+	path := strings.Split(os.Args[0], split)
+
+	if path[len(path)-1] == "server" || path[len(path)-1] == "./server" {
 		servers.Start()
 	} else {
 		clients.Start()
